51066a362b609ff2114899b87173b237b6295f7b
questandachievement <masamudro2001@gmail.com>
2017-09-10 08:10:19 +0700

    Add files via upload

--- 86idlesync.sh
+++ 86idlesync.sh
@@ -2,6 +2,7 @@
 isyncsum=0
 cycle=0
 cyclesample=0
+aheadschedule=0
 while true; do
 cycle=$(( $cycle + 1 ))
 cyclesample=$(( $cyclesample + 1 ))
@@ -33,7 +34,20 @@ if [ $cyclesample -gt $sampleminimum ]
     echo wake coalescing $isync seconds
     if [ ${cpuusage%%.*} -lt "100" ]
       then
+        echo setting aheadschedule
+        asched1=$(( $isync * 2 ))
+        asched2=$(( $isync * 3 ))
+        asched3=$(( $isync * 4 ))
+        asched4=$(( $isync * 5 ))
+        asched5=$(( $isync * 6 ))
+        asched6=$(( $isync * 7 ))
+        echo isyncsched $isync S $asched1 S $asched2 S $asched3 S $asched4 S $asched5 S
         pmset relative wake $isync
+        pmset relative wake $asched1
+        pmset relative wake $asched2
+        pmset relative wake $asched3
+        pmset relative wake $asched4
+        pmset relative wake $asched5
         sleep $isync
         isyncsum=0
         cycle=0
