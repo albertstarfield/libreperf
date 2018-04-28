4afccf7f2ea70109d9cec45272accb8afad9a558
questandachievement <masamudro2001@gmail.com>
2018-04-28 22:03:15 +0700

    Update watchdog.sh

--- watchdog.sh
+++ watchdog.sh
@@ -3,10 +3,10 @@ sleep 60
 while true; do
 if [ ! -d "/libreperfruntime/sys" ] || [ ! -d "/libreperfruntime/binsync" ] || [ ! -d "/libreperfruntime/bin" ] || [ ! -d "/libreperfruntime/subbin" ] || [ ! -d "/libreperfruntime/plugins" ] || [ ! -d "/zramblock0" ]; then
 echo ramdisk kernel integrity failed 0x1
-sudo dtrace -w -n "BEGIN{ panic();}"
+#sudo dtrace -w -n "BEGIN{ panic();}"
 sudo sync &
-sudo purge
-sudo reboot -q
+#sudo purge
+#sudo reboot -q
 
 PROCESS=diskimages-helper
 number=$(ps aux | grep $PROCESS | wc -l)
