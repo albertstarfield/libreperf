2d0ea97116682e9ba48c38395450654b02acaa92
questandachievement <masamudro2001@gmail.com>
2018-04-28 22:09:20 +0700

    Update embryo.sh

--- embryo.sh
+++ embryo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #embryo process
 while true; do
-sudo sh /usr/local/lbpbin/libreperf.sh > /libreperfruntime/runtimedebugmsg.log
+sudo sh /usr/local/lbpbin/libreperf.sh > /var/log/runtimedebugmsg.log
 done
