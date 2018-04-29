6da744f863bfeab626965b0adeb05c04d35b3852
questandachievement <masamudro2001@gmail.com>
2018-04-29 17:38:40 +0700

    Update uiperfpatch.sh

--- uiperfpatch.sh
+++ uiperfpatch.sh
@@ -64,6 +64,29 @@ cd /Users/; for i in *; do sudo -u "$i" defaults write com.apple.dt.Xcode UseSan
 #killall -KILL Dock
 echo phase
 cd /Users/; for i in *; do sudo -u "$i" defaults write com.apple.dock showhidden -bool true; done
+#Disables apple services
+TODISABLE=('com.apple.security.keychainsyncingoveridsproxy' 'com.apple.personad' 'com.apple.passd' 'com.apple.screensharing.MessagesAgent' 'com.apple.CommCenter-osx' 'com.apple.Maps.mapspushd' 'com.apple.Maps.pushdaemon' 'com.apple.photoanalysisd' 'com.apple.telephonyutilities.callservicesd' 'com.apple.AirPlayUIAgent' 'com.apple.AirPortBaseStationAgent' 'com.apple.CalendarAgent' 'com.apple.DictationIM' 'com.apple.iCloudUserNotifications' 'com.apple.familycircled' 'com.apple.familycontrols.useragent' 'com.apple.familynotificationd' 'com.apple.gamed' 'com.apple.icloud.findmydeviced.findmydevice-user-agent' 'com.apple.icloud.fmfd' 'com.apple.imagent' 'com.apple.cloudfamilyrestrictionsd-mac' 'com.apple.cloudpaird' 'com.apple.cloudphotosd' 'com.apple.DictationIM' 'com.apple.assistant_service' 'com.apple.CallHistorySyncHelper' 'com.apple.CallHistoryPluginHelper' 'com.apple.AOSPushRelay' 'com.apple.IMLoggingAgent' 'com.apple.geodMachServiceBridge' 'com.apple.syncdefaultsd' 'com.apple.security.cloudkeychainproxy3' 'com.apple.security.idskeychainsyncingproxy' 'com.apple.security.keychain-circle-notification' 'com.apple.sharingd' 'com.apple.appleseed.seedusaged' 'com.apple.cloudd' 'com.apple.assistantd' 'com.apple.parentalcontrols.check' 'com.apple.parsecd' 'com.apple.identityservicesd')
+
+for agent in "${TODISABLE[@]}"
+do
+    {
+        sudo launchctl unload -w /System/Library/LaunchAgents/${agent}.plist
+        launchctl unload -w /System/Library/LaunchAgents/${agent}.plist
+    } &> /dev/null
+    echo "[OK] Agent ${agent} disabled"
+done
+
+# Daemons to disable
+TODISABLE=('com.apple.netbiosd' 'com.apple.preferences.timezone.admintool' 'com.apple.preferences.timezone.auto' 'com.apple.remotepairtool' 'com.apple.rpmuxd' 'com.apple.security.FDERecoveryAgent' 'com.apple.icloud.findmydeviced' 'com.apple.findmymacmessenger' 'com.apple.familycontrols' 'com.apple.findmymac' 'com.apple.SubmitDiagInfo' 'com.apple.screensharing' 'com.apple.appleseed.fbahelperd' 'com.apple.apsd' 'com.apple.AOSNotificationOSX' 'com.apple.FileSyncAgent.sshd' 'com.apple.ManagedClient.cloudconfigurationd' 'com.apple.ManagedClient.enroll' 'com.apple.ManagedClient' 'com.apple.ManagedClient.startup' 'com.apple.iCloudStats' 'com.apple.locationd' 'com.apple.mbicloudsetupd' 'com.apple.laterscheduler' 'com.apple.awacsd' 'com.apple.eapolcfg_auth' 'com.apple.familycontrols')
+
+for daemon in "${TODISABLE[@]}"
+do
+    {
+        sudo launchctl unload -w /System/Library/LaunchDaemons/${daemon}.plist
+        launchctl unload -w /System/Library/LaunchDaemons/${daemon}.plist
+    } &> /dev/null
+    echo "[OK] Daemon ${daemon} disabled"
+done
     else
       cd /Users/; for i in *; do sudo -u "$i" defaults write com.apple.dock minimize-to-application -bool NO; done
       #UXOptimization
@@ -111,6 +134,29 @@ cd /Users/; for i in *; do sudo -u "$i" defaults write com.apple.dock showhidden
       cd /Users/; for i in *; do sudo -u "$i" defaults write com.apple.dt.Xcode UseSanitizedBuildSystemEnvironment -bool YES; done
       echo phase
       cd /Users/; for i in *; do sudo -u "$i" defaults write com.apple.dock showhidden -bool true; done
+      #enables apple services
+TODISABLE=('com.apple.security.keychainsyncingoveridsproxy' 'com.apple.personad' 'com.apple.passd' 'com.apple.screensharing.MessagesAgent' 'com.apple.CommCenter-osx' 'com.apple.Maps.mapspushd' 'com.apple.Maps.pushdaemon' 'com.apple.photoanalysisd' 'com.apple.telephonyutilities.callservicesd' 'com.apple.AirPlayUIAgent' 'com.apple.AirPortBaseStationAgent' 'com.apple.CalendarAgent' 'com.apple.DictationIM' 'com.apple.iCloudUserNotifications' 'com.apple.familycircled' 'com.apple.familycontrols.useragent' 'com.apple.familynotificationd' 'com.apple.gamed' 'com.apple.icloud.findmydeviced.findmydevice-user-agent' 'com.apple.icloud.fmfd' 'com.apple.imagent' 'com.apple.cloudfamilyrestrictionsd-mac' 'com.apple.cloudpaird' 'com.apple.cloudphotosd' 'com.apple.DictationIM' 'com.apple.assistant_service' 'com.apple.CallHistorySyncHelper' 'com.apple.CallHistoryPluginHelper' 'com.apple.AOSPushRelay' 'com.apple.IMLoggingAgent' 'com.apple.geodMachServiceBridge' 'com.apple.syncdefaultsd' 'com.apple.security.cloudkeychainproxy3' 'com.apple.security.idskeychainsyncingproxy' 'com.apple.security.keychain-circle-notification' 'com.apple.sharingd' 'com.apple.appleseed.seedusaged' 'com.apple.cloudd' 'com.apple.assistantd' 'com.apple.parentalcontrols.check' 'com.apple.parsecd' 'com.apple.identityservicesd')
+
+for agent in "${TODISABLE[@]}"
+do
+    {
+        sudo launchctl load -w /System/Library/LaunchAgents/${agent}.plist
+        launchctl load -w /System/Library/LaunchAgents/${agent}.plist
+    } &> /dev/null
+    echo "[OK] Agent ${agent} disabled"
+done
+
+# Daemons to disable
+TODISABLE=('com.apple.netbiosd' 'com.apple.preferences.timezone.admintool' 'com.apple.preferences.timezone.auto' 'com.apple.remotepairtool' 'com.apple.rpmuxd' 'com.apple.security.FDERecoveryAgent' 'com.apple.icloud.findmydeviced' 'com.apple.findmymacmessenger' 'com.apple.familycontrols' 'com.apple.findmymac' 'com.apple.SubmitDiagInfo' 'com.apple.screensharing' 'com.apple.appleseed.fbahelperd' 'com.apple.apsd' 'com.apple.AOSNotificationOSX' 'com.apple.FileSyncAgent.sshd' 'com.apple.ManagedClient.cloudconfigurationd' 'com.apple.ManagedClient.enroll' 'com.apple.ManagedClient' 'com.apple.ManagedClient.startup' 'com.apple.iCloudStats' 'com.apple.locationd' 'com.apple.mbicloudsetupd' 'com.apple.laterscheduler' 'com.apple.awacsd' 'com.apple.eapolcfg_auth' 'com.apple.familycontrols')
+
+for daemon in "${TODISABLE[@]}"
+do
+    {
+        sudo launchctl load -w /System/Library/LaunchDaemons/${daemon}.plist
+        launchctl load -w /System/Library/LaunchDaemons/${daemon}.plist
+    } &> /dev/null
+    echo "[OK] Daemon ${daemon} disabled"
+done
 fi
 #Resource Window Manager End
 #gift for compositor
